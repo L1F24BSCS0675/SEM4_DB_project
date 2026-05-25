52afaa82992ec4e42909e73bd97f6ff8b1be3350
L1F24BSCS0675 <simraniftikhar14@gmail.com>
2026-05-25 16:27:13 +0500

    file 14

---
+++ delete_category.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+if(!isset($_SESSION['admin_id'])){
+    header("Location: ../auth/login.php");
+    exit();
+}
+include('../config/db.php');
+
+if(!isset($_GET['id']) || empty($_GET['id'])){
+    header("Location: manage_categories.php");
+    exit();
+}
+
+$id = intval($_GET['id']);
+
+// check exists
+$chk = mysqli_prepare($conn, "select id from categories where id = ?");
+mysqli_stmt_bind_param($chk, "i", $id);
+mysqli_stmt_execute($chk);
+mysqli_stmt_store_result($chk);
+
+if(mysqli_stmt_num_rows($chk) == 0){
+    header("Location: manage_categories.php");
+    exit();
+}
+
+// delete category (food items deleted automatically by foreign key cascade)
+$stmt = mysqli_prepare($conn, "delete from categories where id = ?");
+mysqli_stmt_bind_param($stmt, "i", $id);
+mysqli_stmt_execute($stmt);
+
+header("Location: manage_categories.php?msg=deleted");
+exit();
+?>
